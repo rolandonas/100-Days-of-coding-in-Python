4ff9920d7e9255acae8d977ad0881dd635222fce
rolandonas <rolandonas@yahoo.com>
2023-06-20 19:24:31 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+# #1.1 printing
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this")
+# print("Print('what to print')")
+#
+# #1.2 string Manipulation
+# print("Hello world!\nHello word!\nHello word!")
+# print("Hello" + "Roland")
+# print("Hello" +" "+ "Roland") # Adding space to the string
+# #types of error Indentation error(space error) and syntax error (could be the quote)
+# #1.3 Debugging
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the '+' Sign.")
+# print("'e.g'. print(Hello + World)")
+# print("New lines can be created with a backslash and n,")
+# #Input Function
+# input("what is your name?")
+# # I stored Roland in the input file so it returns HelloRoland
+# print("Hello" +input("what is your name?"))
+#Excercise
+#A program that calculate the number of letters in a name
+# input("Hey Dear, what is your name?")
+# print(Len(input("Hey Dear, what is your name?")))
+# input("What is your name?")
+# print(len(input("what is your name?")))
+#1.4 Python Variables
+# Dont change the code below
+# a = input("a:")
+# b = input("b:")
+#######################
+#write your code here
+# c = a
+# a = b
+# b = c
+# print("a =" + b)
+# print("b= " + a)
+#1. Create a greeting for your program.
+print("Welcome to Roland studio generator.")
+#2. Ask the user for the city that they grew up in.
+cityname = input("What city did you grow up in?\n")
+
+#3. Ask the user for the name of a pet.
+petname = input("What is the name of your favourite pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name shold be:\n"  + cityname +  petname)
+#5. Make sure the input cursor shows on a new line:
+
