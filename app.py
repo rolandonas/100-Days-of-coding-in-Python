1c1f849c67c86b3bc1cddaa18a102b10c9bdfdce
rolandonas <rolandonas@yahoo.com>
2023-06-25 21:16:35 -0600

    Day 6

--- app.py
+++ app.py
@@ -425,29 +425,29 @@ scissors = '''
 # print(number)
 
 #excersise  FizzBuzz
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("fizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("fizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
 ##day 5 final assignment
 #password project
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+#
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level
 # password = ""
@@ -464,23 +464,35 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 # print(password)
 
 #Hard Level
-password_list = []
+# password_list = []
+#
+# for char in range(1, nr_letters + 1):
+#   password_list.append(random.choice(letters))
+#
+# for char in range(1, nr_symbols + 1):
+#   password_list += random.choice(symbols)
+#
+# for char in range(1, nr_numbers + 1):
+#   password_list += random.choice(numbers)
+#
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+#
+# password = ""
+# for char in password_list:
+#   password += char
+#
+# print(f"Your password is: {password}")
 
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+####Day 6 ####
+# function
+def my_function():
+    print("hello world")
+    print("My name is roland Onabolujo")
 
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+for intro in range(6):
+    my_function()
 
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
-
-print(password_list)
-random.shuffle(password_list)
-print(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}").
+# indentation
+#while loop
